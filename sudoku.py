44f2ebf1f403cc4c00be28db63b46f6137a1be93
Thomas <dancerfool@gmail.com>
2020-06-24 20:42:36 -0400

    cleaned up and fixed naked single hunting
    cleaner and more legible code, also actually properly hunts for naked singles rather than just checking if it can complete a box/row/col
    
    naked pairs/triplets would be the next thing to find, but much more annoying I think

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,5 @@
+import copy
+
 rows = []
 cols = []
 boxes = []
@@ -54,8 +56,6 @@ def buildIndexHolders():
 
         boxes.append(box)
 
-
-
 def buildPotentials(index):
     rowNum = int(index / 9)
     colNum = index % 9
@@ -81,63 +81,46 @@ def checkPuzzle():
     # for every row/col/box
     for x in range(9):
 
-        potentialIndices = [[], [], [], [], [], [], [], [], [], []]
-        # for every row box
-        for y in range(9 * x, 9 * x + 9):
-            # y represents raw index into array
-            # x offsets it by 9 every iteration, so it starts as 0->8, then 9->18, so on
-            if len(sudokuAr[y]) > 1:
-                for z in sudokuAr[y]:
-                    # if z exists on the potential numbers list, mark index as a potential index for this z
-                    potentialIndices[int(z)].append(y)
+        potentialIndices = [[], [], [], [], [], [], [], [], [], []] 
+        for y in rows[x]:
+            for z in potentialsAr[y]:
+                potentialIndices[z].append(y)
+        for y in range(1, 10):
+            if (len(potentialIndices[y]) == 1):
+                sudokuAr[potentialIndices[y][0]] = y
+                potentialsAr[potentialIndices[y][0]] = [y]
 
-        for y in range(len(potentialIndices)):
-            if len(potentialIndices[y]) == 1:
-                sudokuAr[potentialIndices[y][0]] = [str(y)]
+        potentialIndices = [[], [], [], [], [], [], [], [], [], []] 
+        for y in cols[x]:
+            for z in potentialsAr[y]:
+                potentialIndices[z].append(y)
+        for y in range(1, 10):
+            if (len(potentialIndices[y]) == 1):
+                sudokuAr[potentialIndices[y][0]] = y
+                potentialsAr[potentialIndices[y][0]] = [y]
 
-        potentialIndices = [[], [], [], [], [], [], [], [], [], []]
-        # for every col box
-        for y in range(9):
-            # y represents ith box in column
-            # x represents offset of column
-            if len(sudokuAr[(y * 9) + x]) > 1:
-                for z in sudokuAr[(y * 9) + x]:
-                    # if z exists on the potential numbers list, mark index as a potential index for this z
-                    potentialIndices[int(z)].append((y * 9) + x)
+        potentialIndices = [[], [], [], [], [], [], [], [], [], []] 
+        for y in boxes[x]:
+            for z in potentialsAr[y]:
+                potentialIndices[z].append(y)
+        for y in range(1, 10):
+            if (len(potentialIndices[y]) == 1):
+                sudokuAr[potentialIndices[y][0]] = y
+                potentialsAr[potentialIndices[y][0]] = [y]
 
-        for y in range(len(potentialIndices)):
-            if len(potentialIndices[y]) == 1:
-                sudokuAr[potentialIndices[y][0]] = [str(y)]
-
-        potentialIndices = [[], [], [], [], [], [], [], [], [], []]
-        # for every box's box
-        # x represents boxCenter
-        boxRow = int(x / 3)
-        boxCol = int(x % 3)
-
-        boxCenter = (1 + 3 * boxRow) * 9 + (1 + 3 * boxCol)
-        for y in range(-1, 2):
-            for z in range(-1, 2):
-                if len(sudokuAr[(boxCenter + z) + 9 * y]) > 1:
-                    for a in sudokuAr[(boxCenter + z) + 9 * y]:
-                        # if z exists on the potential numbers list, mark index as a potential index for this z
-                        potentialIndices[int(a)].append((boxCenter + z) + 9 * y)
-
-        for y in range(len(potentialIndices)):
-            if len(potentialIndices[y]) == 1:
-                sudokuAr[potentialIndices[y][0]] = [str(y)]
-        
-
-    return sudokuAr
-
+    # go through each potentials list to clear out singles
+    for x in range(81):
+        if (sudokuAr[x] == 0 and len(potentialsAr[x]) == 1):
+            sudokuAr[x] = potentialsAr[x][0]
 
 def main():
-    print("Type out or copy a string representing a sudoku puzzle, where squares are in order by column left to right, starting with top row and going down. 'M' represents an empty space.")
-    enteredString = input("leave blank or type something wrong and we'll just use the one already typed in as a var")
+    #print("Type out or copy a string representing a sudoku puzzle, where squares are in order by column left to right, starting with top row and going down. 'M' represents an empty space.")
+    enteredString = ""#input("leave blank or type something wrong and we'll just use the one already typed in as a var")
     sudokuString = ""
     if len(enteredString) == 81:
         sudokuString = enteredString
     else:
+        #sudokuString = "M9MMMM2MMMMMMM5M8MMM84MMM1MMM6MM13M5M5MMM97M64MM2MMMMMMMMMM2MM9734MMMMMMMMMM6MMMM"
         #sudokuString = "M7M3M5MM9MMMMMM1M8MMMM9MMMMM3M4MMMMMM98MM2MM77M4MMM9MMMMMMMMMM5MMM68MM436MMMMMMMM"
         sudokuString = "378415962429763185561928374832M5749MMM6MMMM5MMMMMMMM18M8MMMMM3MM57M316M9MM3M4MMM7"
         #sudokuString = "M27154396965M27148341689M525M34682714725M36896189724M578M23591415479M82323984156M"
@@ -150,17 +133,17 @@ def main():
 
     while not solved:
         if (index > 80):
+            checkPuzzle()
             if old == sudokuAr:
                 solved = True
             index = 0
-            #sudokuAr = checkPuzzle(sudokuAr)
-            old = sudokuAr
+            old = copy.deepcopy(sudokuAr)
         if (sudokuAr[index] == 0):
             buildPotentials(index)
-            if (len(potentialsAr[index]) == 1):
-                sudokuAr[index] = potentialsAr[index][0]
         index += 1
 
+    for i in range(9):
+        print(str(potentialsAr[i*9]) + " " + str(potentialsAr[i*9+1]) + " " + str(potentialsAr[i*9+2]) + " " + str(potentialsAr[i*9+3]) + " " + str(potentialsAr[i*9+4]) + " " + str(potentialsAr[i*9+5]) + " " + str(potentialsAr[i*9+6]) + " " + str(potentialsAr[i*9+7]) + " " + str(potentialsAr[i*9+8]))
     printPuzzle()
 
 if __name__ == "__main__":
